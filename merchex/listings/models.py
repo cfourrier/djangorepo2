8fc070c533ca69d746dea0ec9835459cd57f27b5
cfourrier <evient@evient-3.local>
2022-09-12 11:26:06 -0500

    New model added

--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
-# Create your models here.
 # listings/models.py
-
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
+
+class Ad(models.Model):
+    title = models.fields.CharField(max_length=100)
--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -2,9 +2,20 @@
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
+from listings.models import Ad
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :<p>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+""")
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
@@ -13,4 +24,10 @@ def contact(request):
     return HttpResponse('<h1>Contact</h1> <p>Para información</p>')
 
 def listings(request):
-    return HttpResponse('<h1>Listings</h1> <p>Pagina Listings</p>')
+    ads = Ad.objects.all()
+    return HttpResponse(f"""
+        <h2>{ads[0].title}</h2>
+        <h2>{ads[1].title}</h2>
+        <h2>{ads[2].title}</h2>
+        <h2>{ads[3].title}</h2>
+""")
