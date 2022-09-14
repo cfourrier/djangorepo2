6519e59dd40ca4646b5b92aaa844035c034a3e3f
cfourrier <evient@evient-3.local>
2022-09-14 08:36:19 -0500

    ForeignKey added

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,21 @@
+# listings/admin.py
+
 from django.contrib import admin
 
-# Register your models here.
+from listings.models import Band
+#from bands.models import Band
+from listings.models import Ad
+from listings.models import Productos
+
+
+#admin.site.register(Band)
+
+class BandAdmin(admin.ModelAdmin):  # nous insérons ces deux lignes..
+    list_display = ('name', 'genre') # liste les champs que nous voulons sur l'affichage de la liste
+
+class ProductoAdmin(admin.ModelAdmin):  # nous insérons ces deux lignes..
+    list_display = ('description', 'type', 'sold') # liste les champs que nous voulons sur l'affichage de la liste
+
+admin.site.register(Band, BandAdmin) # nous modifions cette ligne, en ajoutant un deuxième argument
+admin.site.register(Ad)
+admin.site.register(Productos, ProductoAdmin)
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -2,7 +2,30 @@ from django.db import models
 
 # listings/models.py
 class Band(models.Model):
+    def __str__(self):
+        return f'{self.name}'
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+
 
 class Ad(models.Model):
     title = models.fields.CharField(max_length=100)
+
+class Productos(models.Model):
+    class Type(models.TextChoices):
+        RECORDS = 'records'
+        CLOTHING = 'clothings'
+        MISCELLANEOUS = 'miscellaneous'
+    description = models.fields.CharField(max_length=100)
+    sold = models.fields.BooleanField(default=False)
+    type = models.fields.CharField(choices=Type.choices, max_length=30)
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+
