7d281f9c93e4946a62ce4f717f231170323b2745
cfourrier <evient@evient-3.local>
2022-09-15 11:50:38 -0500

    band_detail.html added

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,11 +5,17 @@ from django.shortcuts import render
 from listings.models import Band
 from listings.models import Ad
 
-def hello(request):
-    bands = Band.objects.all()
-    return render(request,
-    'listings/hello.html',
-    {'bands': bands})
+def band_list(request):  # renommer la fonction de vue
+   bands = Band.objects.all()
+   return render(request,
+           'listings/band_list.html',  # pointe vers le nouveau nom de modèle
+           {'bands': bands})
+
+def band_detail(request, id):
+  band = Band.objects.get(id=id)  # nous insérons cette ligne pour obtenir le Band avec cet id
+  return render(request,
+          'listings/band_detail.html',
+          {'band': band}) # nous mettons à jour cette ligne pour passer le groupe au gabarit
 
 def about(request):
     bands = Band.objects.all()
